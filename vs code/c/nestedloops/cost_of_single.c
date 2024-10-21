5592b750f83164c1ecf2992194c6ed4205162964
shajith <shajith@shajith-kali>
2024-10-21 23:58:39 +0530

    Added DSA notes of IITD

--- vs code/c/nestedloops/cost_of_single.c
+++ vs code/c/nestedloops/cost_of_single.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int total_selling,profit;
+int main()
+{
+    int total_selling, profit;
     printf("Enter the total selling prize of the 15 items : \n");
-    scanf("%d",&total_selling);
+    scanf("%d", &total_selling);
     printf("Enter the profit  you generated : \n");
-    scanf("%d",&profit);
-    printf("The prize of each item is %d \n",(total_selling - profit)/15);
-    
+    scanf("%d", &profit);
+    printf("The prize of each item is : %d \n", (total_selling - profit) / 15);
+
     return 0;
 }
